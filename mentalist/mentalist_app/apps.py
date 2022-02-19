017baa4998cd3f8805e64cf3adbae0e7c5e3fb38
hathun <hathun@sfu.ca>
2022-02-19 13:02:21 -0800

    Set up project: connect React app and Django REST server

---
+++ mentalist/mentalist_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MentalistAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mentalist_app'
---
+++ mentalist/webapp/src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App = (props) => {
+    return (
+        <>
+            <h1> Hello World </h1>
+            <h2>This is Django REST API and React app</h2>
+        </>
+    );
+}
