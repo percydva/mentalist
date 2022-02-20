6d649952f9b21ebb1a5f67a59c718e68e2e7c6fb
Long K. Tran <longtr213@gmail.com>
2022-02-19 17:00:34 -0800

    convert to react ts

--- mentalist/webapp/src/app.tsx
+++ mentalist/webapp/src/app.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const App = (props) => {
     return (
         <>
-            <h1> Hello World </h1>
+            <h1> Hello World 2</h1>
             <h2>This is Django REST API and React app</h2>
         </>
     );
